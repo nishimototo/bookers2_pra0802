831dfc17fed9f41115863bbfaf6af4a25929405a
EC2 Default User <ec2-user@ip-172-31-40-138.ap-northeast-1.compute.internal>
2021-08-03 00:02:52 +0000

    コメント・いいね非同期

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
   get "home/about" => "homes#about"
+  get "search" => "searches#search"
+
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
